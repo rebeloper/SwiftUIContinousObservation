19a0cb514c31f88a1b981dbb351b56ff2ce4833b
Alex Nagy <rebeloperalpar@gmail.com>
2024-05-28 12:57:23 +0300

    first commit

--- Sources/SwiftUIContinousObservation/SwiftUIContinousObservation.swift
+++ Sources/SwiftUIContinousObservation/SwiftUIContinousObservation.swift
@@ -1,2 +1,36 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+
+import SwiftUI
+
+struct ContinousObservationModifier<T>: ViewModifier {
+    
+    init(_ value: T, perform: @escaping (T) -> ()) {
+        withContinousObservation(of: value, perform: perform)
+    }
+    
+    func body(content: Content) -> some View {
+        content
+    }
+    
+    func withContinousObservation(of value: @escaping @autoclosure () -> T, perform: @escaping (T) -> Void) {
+        withObservationTracking {
+            perform(value())
+        } onChange: {
+            Task { @MainActor in
+                withContinousObservation(of: value(), perform: perform)
+            }
+        }
+    }
+}
+
+public extension View {
+    /// Adds a modifier for this view that fires an action when a specific
+    /// `@Observable` value changes.
+    /// - Parameters:
+    ///   - value: The value to check against when determining whether
+    ///     to run the closure.
+    ///   - perform: A closure to run when the value changes.
+    /// - Returns: A view that fires an action when the specified value changes.
+    func onObservedChange<T>(of value: T, perform: @escaping (T) -> ()) -> some View {
+        self.modifier(ContinousObservationModifier(value, perform: perform))
+    }
+}
--- Tests/SwiftUIContinousObservationTests/SwiftUIContinousObservationTests.swift
+++ Tests/SwiftUIContinousObservationTests/SwiftUIContinousObservationTests.swift
@@ -2,11 +2,4 @@ import XCTest
 @testable import SwiftUIContinousObservation
 
 final class SwiftUIContinousObservationTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
 }
