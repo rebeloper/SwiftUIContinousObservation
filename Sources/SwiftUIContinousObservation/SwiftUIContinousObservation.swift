74934d518714a51821c53adcae3a21483523929a
Alex Nagy <rebeloperalpar@gmail.com>
2024-06-14 11:18:13 +0300

    optional

--- Sources/SwiftUIContinousObservation/SwiftUIContinousObservation.swift
+++ Sources/SwiftUIContinousObservation/SwiftUIContinousObservation.swift
@@ -24,6 +24,29 @@ struct ContinousObservationModifier<T>: ViewModifier {
     }
 }
 
+struct ContinousOptionalObservationModifier<T>: ViewModifier {
+    
+    init(_ value: T?, perform: @escaping (T?) async -> ()) {
+        withContinousObservation(of: value, perform: perform)
+    }
+    
+    func body(content: Content) -> some View {
+        content
+    }
+    
+    func withContinousObservation(of value: @escaping @autoclosure () -> T?, perform: @escaping (T?) async -> Void) {
+        let _ = withObservationTracking {
+            Task { @MainActor in
+                await perform(value())
+            }
+        } onChange: {
+            Task { @MainActor in
+                withContinousObservation(of: value(), perform: perform)
+            }
+        }
+    }
+}
+
 public extension View {
     /// Adds a modifier for this view that fires an action when a specific
     /// `@Observable` value changes.
@@ -34,5 +57,16 @@ public extension View {
     /// - Returns: A view that fires an action when the specified value changes.
     func onChangeOf<T>(_ value: T, perform: @escaping (T) async -> ()) -> some View {
         self.modifier(ContinousObservationModifier(value, perform: perform))
+    }
+    
+    /// Adds a modifier for this view that fires an action when a specific optional
+    /// `@Observable` value changes.
+    /// - Parameters:
+    ///   - value: The optional value to check against when determining whether
+    ///     to run the closure.
+    ///   - perform: An async closure to run when the value changes.
+    /// - Returns: A view that fires an action when the specified value changes.
+    func onChangeOfOptional<T>(_ value: T?, perform: @escaping (T?) async -> ()) -> some View {
+        self.modifier(ContinousOptionalObservationModifier(value, perform: perform))
     }
 }
