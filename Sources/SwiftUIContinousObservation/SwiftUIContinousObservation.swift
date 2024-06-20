17d8147c08cca7aca6b6bc47b41127319a7763f8
Alex Nagy <rebeloperalpar@gmail.com>
2024-06-20 13:02:13 +0300

    refactroing

--- Sources/SwiftUIContinousObservation/SwiftUIContinousObservation.swift
+++ Sources/SwiftUIContinousObservation/SwiftUIContinousObservation.swift
@@ -3,29 +3,6 @@ import SwiftUI
 
 struct ContinousObservationModifier<T>: ViewModifier {
     
-    init(_ value: T, perform: @escaping (T) async -> ()) {
-        withContinousObservation(of: value, perform: perform)
-    }
-    
-    func body(content: Content) -> some View {
-        content
-    }
-    
-    func withContinousObservation(of value: @escaping @autoclosure () -> T, perform: @escaping (T) async -> Void) {
-        let _ = withObservationTracking {
-            Task { @MainActor in
-                await perform(value())
-            }
-        } onChange: {
-            Task { @MainActor in
-                withContinousObservation(of: value(), perform: perform)
-            }
-        }
-    }
-}
-
-struct ContinousOptionalObservationModifier<T>: ViewModifier {
-    
     init(_ value: T?, perform: @escaping (T?) async -> ()) {
         withContinousObservation(of: value, perform: perform)
     }
@@ -48,16 +25,6 @@ struct ContinousOptionalObservationModifier<T>: ViewModifier {
 }
 
 public extension View {
-    /// Adds a modifier for this view that fires an action when a specific
-    /// `@Observable` value changes.
-    /// - Parameters:
-    ///   - value: The value to check against when determining whether
-    ///     to run the closure.
-    ///   - perform: An async closure to run when the value changes.
-    /// - Returns: A view that fires an action when the specified value changes.
-    func onReceive<T>(of value: T, perform: @escaping (T) async -> ()) -> some View {
-        self.modifier(ContinousObservationModifier(value, perform: perform))
-    }
     
     /// Adds a modifier for this view that fires an action when a specific
     /// `@Observable` optional value changes.
@@ -67,6 +34,20 @@ public extension View {
     ///   - perform: An async closure to run when the optional value changes.
     /// - Returns: A view that fires an action when the specified optional value changes.
     func onReceive<T>(of value: T?, perform: @escaping (T?) async -> ()) -> some View {
-        self.modifier(ContinousOptionalObservationModifier(value, perform: perform))
+        self.modifier(ContinousObservationModifier(value, perform: perform))
+    }
+    
+    /// Adds a modifier for this view that fires an action when a specific
+    /// `@Observable` value changes.
+    /// - Parameters:
+    ///   - value: The value to check against when determining whether
+    ///     to run the closure.
+    ///   - perform: An async closure to run when the value changes.
+    /// - Returns: A view that fires an action when the specified value changes.
+    func onReceive<T>(of value: T, perform: @escaping (T) async -> ()) -> some View {
+        self.modifier(ContinousObservationModifier(value, perform: { value in
+            guard let value else { return }
+            await perform(value)
+        }))
     }
 }
